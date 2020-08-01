d898849dd30e82d4d2b4648f447bc295b0f06488
neha <nehanirmal11@gmail.com>
2020-08-01 18:23:46 +0530

    reverse commit of develop

---
+++ src/test/java/qaclickacademy/demo_branching.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+	import org.testng.annotations.Test;
+
+		public class demo_branching{
+			@Test
+			public void NativeAPpAndroid()
+			{
+				System.out.println("NativeAPpAndroid");
+			}
+			
+			@Test
+			public void IOSApps()
+			{
+				System.out.println("IOSApps");	
+			}
+
+	}
